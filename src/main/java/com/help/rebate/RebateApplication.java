510dd69fe91b4eb582c23d553f5f4368f5060de8
ZFCISBEST <ZFCISBEST@163.com>
2021-11-13 18:28:24 +0800

    init

---
+++ src/main/java/com/help/rebate/RebateApplication.java
@@ -0,0 +1,13 @@
+package com.help.rebate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RebateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RebateApplication.class, args);
+	}
+
+}

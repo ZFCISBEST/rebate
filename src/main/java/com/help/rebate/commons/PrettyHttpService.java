53f35192156fb7289dfb78d1076863406d0dd046
hokxi <812435489@qq.com>
2022-03-06 18:27:53 +0800

    新增大淘客接口

--- src/main/java/com/help/rebate/commons/PrettyHttpService.java
+++ src/main/java/com/help/rebate/commons/PrettyHttpService.java
@@ -13,6 +13,7 @@ import org.springframework.web.client.RestTemplate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 import java.util.stream.Collectors;
 
 /**
@@ -37,7 +38,7 @@ public class PrettyHttpService {
      * @param paramMap
      * @return
      */
-    public String get(String url, Map<String, Object> paramMap) {
+    public String get(String url, TreeMap<String,String> paramMap) {
         //请求
         String param = convertUrlParam(paramMap);
         String newUrl = url + "?" + param;
@@ -53,6 +54,21 @@ public class PrettyHttpService {
         throw new RuntimeException(responseEntity.getStatusCode().getReasonPhrase());
     }
 
+    public String get(String url, Map<String, Object> paramMap) {
+        //请求
+        String param = convertUrlParam(paramMap);
+        String newUrl = url + "?" + param;
+        ResponseEntity<String> responseEntity = restTemplate.getForEntity(newUrl, String.class, paramMap);
+
+        //判定是否正确
+        int statusCodeValue = responseEntity.getStatusCodeValue();
+        if (statusCodeValue == 200) {
+            return responseEntity.getBody();
+        }
+
+        //否则抛出异常
+        throw new RuntimeException(responseEntity.getStatusCode().getReasonPhrase());
+    }
     /**
      * post请求
      * @param url
@@ -84,6 +100,15 @@ public class PrettyHttpService {
     private String convertUrlParam(Map<String, Object> paramMap) {
         List<String> params = new ArrayList<String>();
         for (Map.Entry<String, Object> keyValue : paramMap.entrySet()) {
+            params.add(keyValue.getKey() + "=" + "{" + keyValue.getKey() + "}");
+        }
+
+        String param = params.stream().collect(Collectors.joining("&"));
+        return param;
+    }
+    private String convertUrlParam(TreeMap<String,String> paramMap) {
+        List<String> params = new ArrayList<String>();
+        for (Map.Entry<String,String> keyValue : paramMap.entrySet()) {
             params.add(keyValue.getKey() + "=" + "{" + keyValue.getKey() + "}");
         }
 
---
+++ src/main/java/com/help/rebate/service/dtk/tb/DtkItemConverter.java
@@ -0,0 +1,84 @@
+package com.help.rebate.service.dtk.tb;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.help.rebate.commons.DtkConfig;
+import com.help.rebate.commons.PrettyHttpService;
+import com.help.rebate.utils.EmptyUtils;
+import com.help.rebate.utils.dtk.SignMD5Util;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+/**
+ * 大淘客商品转链接
+ * 参考文档 - https://www.dingdanxia.com/doc/3/8
+ * @author zfcisbest
+ * @date 21/11/14
+ */
+@Service
+public class DtkItemConverter {
+    private static final Logger logger = LoggerFactory.getLogger(DtkItemConverter.class);
+
+    /**
+     * http服务
+     */
+    @Autowired
+    private PrettyHttpService prettyHttpService;
+
+    /**
+     * 淘口令转商品id
+     * @param tkl
+     * @return
+     */
+    public JSONObject parseTkl(String tkl) {
+        //基础参数
+        TreeMap<String,String> params = new TreeMap<String,String>();
+        params.put("appKey", DtkConfig.dtkAppkey);
+        params.put("content", tkl);
+        params.put("version", "v1.0.0");
+        params.put("sign", SignMD5Util.getSignStr(params,DtkConfig.dtkAppsecret));
+
+        String result = prettyHttpService.get(DtkConfig.DTK_PARSE_CONTENT, params);
+//        String result = ApiClient.sendReq(DtkConfig.DTK_PARSE_CONTENT, DtkConfig.dtkAppsecret, params);
+
+//        String appKey = DtkConfig.dtkAppkey;
+//        String appSecret = DtkConfig.dtkAppsecret;
+//        DtkApiClient client = DtkApiClient.getInstance(appKey,appSecret);
+//        DtkParseContentRequest request = new DtkParseContentRequest();
+//        request.setVersion("v1.0.0");
+//        request.setContent(tkl);
+//        DtkApiResponse<DtkParseContentResponse> execute = client.execute(request);
+        return JSON.parseObject(String.valueOf(result));
+    }
+
+    public JSONObject getPrivilegeTkl(String tkl, String relationId, String specialId, String externalId, String pubSite){
+        String goodsId = parseTkl(tkl).getJSONObject("data").getString("goodsId");
+        //基础参数
+        TreeMap<String,String> params = new TreeMap<String,String>();
+        params.put("appKey", DtkConfig.dtkAppkey);
+        params.put("goodsId", goodsId);
+        params.put("version", "v1.3.1");
+
+        //关于ID的参数
+        if (!EmptyUtils.isEmpty(relationId)) {
+            params.put("relation_id", relationId);
+        }
+        if (!EmptyUtils.isEmpty(specialId)) {
+            params.put("special_id", specialId);
+        }
+        if (!EmptyUtils.isEmpty(externalId)) {
+            params.put("external_id", externalId);
+        }
+
+        params.put("sign", SignMD5Util.getSignStr(params,DtkConfig.dtkAppsecret));
+        String result = prettyHttpService.get(DtkConfig.DTK_GET_PRIVILEGE_TKL, params);
+        return JSON.parseObject(String.valueOf(result));
+    }
+
+}

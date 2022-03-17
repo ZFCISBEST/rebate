de1174add1a81e211fd67c2c8c8c61a1a2963fd6
ZFCISBEST <ZFCISBEST@163.com>
2022-03-17 23:13:57 +0800

    rebuild

--- src/main/java/com/help/rebate/service/OrderBindService.java
+++ src/main/java/com/help/rebate/service/OrderBindService.java
@@ -103,22 +103,24 @@ public class OrderBindService {
     }
 
     /**
+     * 根据需要，做绑定的更新
+     * 如果订单状态还没有结束，那么需要前向走，更新订单状态
+     * 如果我们平台给用户的状态已经结算，或者关闭，那么不用更新
+     * 每次更新的时候，主要更新一下其他字段（如预期返利）
+     * 应该多个状态，就是如果实际已经返利给用户了，但是订单状态发生了商家维权，造成我们自己资损，需要记录下来实际给用户反了多少，后面要补回来
+     * @param orderOpenidMap
+     * @param orderDetail
+     */
+    private void updateOrderOpenidMapIfNeeded(OrderOpenidMap orderOpenidMap, OrderDetail orderDetail) {
+
+    }
+
+    /**
      * 根据给定的绑定关系，将已知的其他订单绑定到相同的用户信息上
      * @param orderOpenidMap
      * @param tradeId2OrderDetailMap
      */
     private void createOrderOpenidMapBy(OrderOpenidMap orderOpenidMap, Map<String, OrderDetail> tradeId2OrderDetailMap) {
-
-    }
-
-    /**
-     * 会员用户，通过specialId进行绑定
-     * @param orderDetailList
-     */
-    private void bindBySpecialId(List<OrderDetail> orderDetailList) {
-        //第一种，这里的所有商品，至少有被转链过，那么查出来，那么这种情况，是可以建立openid和specialid的关系并存入用户表的
-
-        //第二种，这里所有的商品，都没有被转链过，那么只能存入specialid字段，其他openid这些数据不填写，mapType就是specialid，表示只是会员
 
     }
 
@@ -131,15 +133,13 @@ public class OrderBindService {
     }
 
     /**
-     * 根据需要，做绑定的更新
-     * 如果订单状态还没有结束，那么需要前向走，更新订单状态
-     * 如果我们平台给用户的状态已经结算，或者关闭，那么不用更新
-     * 每次更新的时候，主要更新一下其他字段（如预期返利）
-     * 应该多个状态，就是如果实际已经返利给用户了，但是订单状态发生了商家维权，造成我们自己资损，需要记录下来实际给用户反了多少，后面要补回来
-     * @param orderOpenidMap
-     * @param orderDetail
+     * 会员用户，通过specialId进行绑定
+     * @param orderDetailList
      */
-    private void updateOrderOpenidMapIfNeeded(OrderOpenidMap orderOpenidMap, OrderDetail orderDetail) {
+    private void bindBySpecialId(List<OrderDetail> orderDetailList) {
+        //第一种，这里的所有商品，至少有被转链过，那么查出来，那么这种情况，是可以建立openid和specialid的关系并存入用户表的
+
+        //第二种，这里所有的商品，都没有被转链过，那么只能存入specialid字段，其他openid这些数据不填写，mapType就是specialid，表示只是会员
 
     }
 }
